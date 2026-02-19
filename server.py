8b6b098e48b3fd2c6d28e086ed93a53de72df508
Shadow821 <gregoyaga@gmail.com>
2026-02-19 17:09:38 +0100

    ajout des espace

--- server.py
+++ server.py
@@ -33,9 +33,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 s.listen()
 print(baner)
-print("::::::::::::::::::::::::::::::::::::::::::::::")
+print(":::::::::::::::::::::::::::::::::::::::::::::: \n")
 print(GREEN + "Le serveur est prêt à accepter les connexions sur {}:{}".format(host, port))
-
+print("\n::::::::::::::::::::::::::::::::::::::::::::::")
 
 # Acceptation des clients
 try:
